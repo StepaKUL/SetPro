720b1d7f19ca025a5a63800d2a790201de102e34
StepaKUL <kulik-stepa@mail.ru>
2024-03-15 11:29:45 +0500

    1

---
+++ kulik-rest-api/dbConnection.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+const db_connection = mysql
+ .createConnection({
+ host: "", // имя хоста
+ user: "root", // имя пользователя
+ database: "pe-11_kulik", // имя базы данных
+ password: "", // пароль от базы данных
+ })
+ .on("error", (err) => {
+ console.log("Failed to connect to Database - ", err);
+ });
+module.exports = db_connection;
